94d7d30db596dc25ec64a1cb95689e8ca74fcf07
Usman Nuhu Adamu <150130727+Useynuh@users.noreply.github.com>
2026-08-19 11:44:06 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,4 @@
-#!/bin/bash set -e php artisan migrate --force exec apache2-foreground This script does two things when your app starts: it creates the missing database tables (migrate --force)
+#!/bin/bash 
+set -e 
+php artisan migrate --force 
+exec apache2-foreground
